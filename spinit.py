164456fbc894bb24fa2fb61539debed0222fa287
Glen Baker <iepathos@gmail.com>
2017-06-06 22:54:06 -0400

    Code cleanup and reorganization

--- spinit.py
+++ spinit.py
@@ -13,28 +13,6 @@ Usage:
 Written By: Glen Baker <iepathos@gmail.com>
 """
 
-parser = argparse.ArgumentParser(description='Generate Nginx Configs from data yaml')
-parser.add_argument('data', help='Input data.yml config')
-opts = parser.parse_args()
-
-with open(opts.data, 'r') as stream:
-    try:
-        data = yaml.load(stream)
-    except yaml.YAMLError as exc:
-        print(exc)
-
-ports = data.get('ports')
-services = data.get('services')
-available_ranges = ports.get('available_ranges')
-base_range = int(ports.get('base'))
-
-# shove an int for every port into a list so its easier to deal with
-lports = []
-for port_range in available_ranges:
-    start, end = port_range.split('-')
-    lports.extend(list(range(base_range + int(start), base_range + int(end) + 1)))
-backup_ports = lports
-
 
 def find_available_regions(lports):
     # group continuous regions together
@@ -44,7 +22,7 @@ def find_available_regions(lports):
     return regions
 
 
-def find_region(regions, chunk):
+def find_region(regions, chunk, available_ranges):
     # find smallest region of required chunk size
     available_regions = [r for r in regions if len(r) >= chunk]
     if len(available_regions) > 0:
@@ -58,113 +36,123 @@ def find_region(regions, chunk):
     return []
 
 
-regions = find_available_regions(lports)
-service_port_map = {service.get('name'): None for service in services}
-component_service_map = {}
-nginx_app_file = open("nginx_service.conf").read()
+def write_config(data):
+    ports = data.get('ports')
+    services = data.get('services')
+    available_ranges = ports.get('available_ranges')
+    base_range = int(ports.get('base'))
 
-# sort services by num_ports beforehand so we allocate smallest services first
-# the Bonus on the challenge requests as many services as possible
-# so that is why prioritize smaller services here rather than some other metric
-# like maximize ports used or components deployed
-services.sort(key=lambda x: int(x.get('num_ports')), reverse=False)
+    # shove an int for every port into a list so its easier to deal with
+    lports = []
+    for port_range in available_ranges:
+        start, end = port_range.split('-')
+        lports.extend(list(range(base_range + int(start), base_range + int(end) + 1)))
 
-# using first available port for the registrar service
-p = lports.pop(0)
-service_port_map['registrar'] = [p]
-try:
-    os.mkdir('nginx_conf')
-except OSError as e:
-    print(e)
-
-conf = nginx_app_file.format(ports=p, name='registrar', components='')
-open("nginx_conf/{}.conf".format('registrar'), "w+").write(conf)
-
-for service in services:
-    name = service.get('name').lower()
-    num_ports = service.get('num_ports')
-    components = service.get('components')
-    conf_ports = []
     regions = find_available_regions(lports)
-    cports = [c.get('port') for c in components if c.get('port') >= num_ports]
-    # some components have port offsets greater than the service's
-    # required ports. At least with the given dataset, increasing
-    # the required ports to match greatest component port
-    # requirements + 1 here allows additional components to be deployed
-    # and increases ports assigned without reducing number of services
-    # if len(cports) > 0:
-    #     num_ports = cports[0] + 1
-    available_region = find_region(regions, num_ports)
+    service_port_map = {service.get('name'): None for service in services}
+    component_service_map = {}
+    nginx_app_file = open("nginx_service.conf").read()
 
-    if len(available_region) > num_ports:
-        service_port_map[name] = available_region[:num_ports]
-        port_offsets = []
-        component_names = []
-        if num_ports > 1:
-            for component in components:
-                if len(available_region) == 1:
-                    # port offset 0 is saved for the service itself, not components
-                    print('WARNING: Port conflict for component %s, skipping.' % component.get('name'))
-                else:
-                    offset = component.get('port')
-                    if offset >= num_ports:
-                        # some components have port offsets greater than the service's
-                        # required ports.
-                        print('WARNING: Port conflict for component %s, port offset greater than required ports for service, skipping.' % component.get('name'))
-                    elif offset not in port_offsets and offset < num_ports:
-                        port_offsets.append(offset)
-                        component_names.append(component.get('name').lower())
-                    else:
-                        print('WARNING: Port conflict for component %s, skipping.' % component.get('name'))
-        else:
-            print('WARNING: Port conflict for service %s, skipping components.' % name)
+    # sort services by num_ports beforehand so we allocate smallest services first
+    # the Bonus on the challenge requests as many services as possible
+    # so that is why prioritize smaller services here rather than some other metric
+    # like maximize ports used or components deployed
+    services.sort(key=lambda x: int(x.get('num_ports')), reverse=False)
 
-        # allocate ports to components
-        cpayload = ''
-        for idx, cname in enumerate(component_names):
-            port = port_offsets[idx] + available_region[0]
-            component_payload = """
-server {{
-    server_name {name};
-    listen {port};
-    root /var/www/{name};
+    # using first available port for the registrar service
+    num_ports = 1
+    regions = find_available_regions(lports)
+    available_region = find_region(regions, num_ports, available_ranges)
+    port = available_region[0]
+    lports.remove(port)
+    service_port_map['registrar'] = [port]
+    try:
+        os.mkdir('nginx_conf')
+    except OSError as e:
+        print(e)
 
-    location / {{
-        index index.htm index.html;
-    }}
-}}
-""".format(name=cname.lower(), port=port)
-            cpayload += component_payload
-            service_port_map[cname] = [port]
-            component_service_map[cname] = name
-            available_region.remove(port)  # port no longer available
-            lports.remove(port)
+    conf = nginx_app_file.format(ports=port, name='registrar', components='')
+    open("nginx_conf/{}.conf".format('registrar'), "w+").write(conf)
 
-        # fill rest of the required ports for service with default
-        # up to port requirement
-        for p in range(num_ports - len(component_names)):
-            conf_ports.append(str(available_region[p]))
-            lports.remove(available_region[p])
-        ports = ';\n    listen '.join(conf_ports)
-        conf = nginx_app_file.format(ports=ports, name=name, components=cpayload)
-        open("nginx_conf/{}.conf".format(name), "w+").write(conf)
-
+    for service in services:
+        name = service.get('name').lower()
+        num_ports = service.get('num_ports')
+        components = service.get('components')
+        conf_ports = []
         regions = find_available_regions(lports)
-    else:
-        print('WARNING: %s ports needed for service %s but not enough continuous ports left, skipping service.' % (num_ports, name))
+        available_region = find_region(regions, num_ports, available_ranges)
+
+        if len(available_region) > num_ports:
+            service_port_map[name] = available_region[:num_ports]
+            port_offsets = []
+            component_names = []
+            if num_ports > 1:
+                for component in components:
+                    if len(available_region) == 1:
+                        # port offset 0 is saved for the service itself, not components
+                        print('WARNING: Port conflict for component %s, skipping.' % component.get('name'))
+                    else:
+                        offset = component.get('port')
+                        if offset >= num_ports:
+                            # some components have port offsets greater than the service's
+                            # required ports.
+                            print('WARNING: Port conflict for component %s, port offset greater than required ports for service, skipping.' % component.get('name'))
+                        elif offset not in port_offsets and offset < num_ports:
+                            port_offsets.append(offset)
+                            component_names.append(component.get('name').lower())
+                        else:
+                            print('WARNING: Port conflict for component %s, skipping.' % component.get('name'))
+            else:
+                print('WARNING: Port conflict for service %s, skipping components.' % name)
+
+            # allocate ports to components
+            cpayload = ''
+            for idx, cname in enumerate(component_names):
+                port = port_offsets[idx] + available_region[0]
+                component_payload = """
+    server {{
+        server_name {name};
+        listen {port};
+        root /var/www/{name};
+
+        location / {{
+            index index.htm index.html;
+        }}
+    }}
+    """.format(name=cname.lower(), port=port)
+                cpayload += component_payload
+                service_port_map[cname] = [port]
+                component_service_map[cname] = name
+                available_region.remove(port)  # port no longer available
+                lports.remove(port)
+
+            # fill rest of the required ports for service with default
+            # up to port requirement
+            for p in range(num_ports - len(component_names)):
+                conf_ports.append(str(available_region[p]))
+                lports.remove(available_region[p])
+            ports = ';\n    listen '.join(conf_ports)
+            conf = nginx_app_file.format(ports=ports, name=name, components=cpayload)
+            open("nginx_conf/{}.conf".format(name), "w+").write(conf)
+
+            regions = find_available_regions(lports)
+        else:
+            print('WARNING: %s ports needed for service %s but not enough continuous ports left, skipping service.' % (num_ports, name))
 
 
-service_check = {}
-for k, v in service_port_map.items():
-    if v is not None:
-        service_check[k] = v
-print('%s services and components allocated' % len(service_check))
-print('Unassigned ports: %s' % lports)
+    service_check = {}
+    for k, v in service_port_map.items():
+        if v is not None:
+            service_check[k] = v
+    print('%s services and components allocated' % len(service_check))
+    print('Unassigned ports: %s' % lports)
 
-try:
-    os.mkdir('nginx_www')
-except OSError as e:
-    print(e)
+    try:
+        os.mkdir('nginx_www')
+    except OSError as e:
+        print(e)
+
+    return service_check, component_service_map
 
 
 # create service index
@@ -234,5 +222,16 @@ def create_services(service_port_map, component_service_map):
                 out.write(html)
 
 
-create_registrar(service_check, component_service_map)
-create_services(service_check, component_service_map)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generate nginx configs and content from data yaml')
+    parser.add_argument('data', help='Input data.yml config')
+    opts = parser.parse_args()
+    with open(opts.data, 'r') as stream:
+        try:
+            data = yaml.load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+    service_check, component_service_map = write_config(data)
+    create_registrar(service_check, component_service_map)
+    create_services(service_check, component_service_map)
+    print('Index URL: http://127.0.0.1:%s/' % service_check['registrar'][0])
